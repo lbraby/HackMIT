525da01d465ba9b52f48092396656e4d1e1c572e
Matthew Carbonaro <mcarbona@nd.edu>
2022-10-01 17:54:50 -0400

    parser for sql and sql requests

--- scripts/parser.py
+++ scripts/parser.py
@@ -1,16 +1,8 @@
-import pandas as pd
-import ijson
-import connections
-
-
-def import_data(path: str):
-    fd = open(path, "r")
-    result = ijson.items(fd, 'item')
-    return result
+import sqlRequests as sql
 
 def load_man(data_gen, conn):
     cursor = conn.cursor()
-    fieldnamesql = "INSERT INTO DAWN_EVENT (timestamp, userId, platform, x, y, z, country, event_name, event_num) "
+    fieldnamesql = "INSERT INTO dawn_event (timestamp, userId, platform, x, y, z, country, event_name, event_num) "
     for item in data_gen:
         try:
             valsql = fieldnamesql + "VALUES ("
@@ -24,6 +16,19 @@ def load_man(data_gen, conn):
             valsql += "'" + list(item.get("events", {"NONE": "NONE"}).keys())[0] + "', "
             valsql += str(list(item.get("events", {"NONE": "NONE"}).values())[0]) + ")"
             cursor.execute(valsql)
+        except IndexError:
+            valsql = fieldnamesql + "VALUES ("
+            valsql += str(item.get("time")) + ", "
+            valsql += str(item.get("userId")) + ", "
+            valsql += "'" + item.get("platform") + "', "
+            valsql += str(item.get("position")[0]) + ", "
+            valsql += str(item.get("position")[1]) + ", "
+            valsql += str(item.get("position")[2]) + ", "
+            valsql += "'" + item.get("country") + "', "
+            valsql += "'NONE', "
+            valsql += str(0) + ")"
+            cursor.execute(valsql)
+            continue
         except Exception as exp:
             print(exp)
             print(item)
@@ -33,8 +38,8 @@ def load_man(data_gen, conn):
 
 
 def main():
-    result = import_data("./data/dawn-event-data.json")
-    connection = connections.run()
+    result = sql.import_data("./data/dawn-event-data.json")
+    connection = sql.MySQL_connect("128.199.35.33", "HackMIT")
     load_man(result, connection)
 
 main()
